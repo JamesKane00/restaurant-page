67312e9d4cbd7775677bd6520397378aa6e23f47
James Kane <james.kane59@outlook.com>
2022-11-25 12:57:42 +0000

    refactoring menu.js, less code duplication

--- src/menu.js
+++ src/menu.js
@@ -12,90 +12,50 @@ export default function renderMenu() {
 
 	// create recipes imgs, containers and ingredients list
 
-	// recipe 1 element creation
-	const containerOne = document.createElement('div');
-	containerOne.classList.add('recipe-container', 'container-one');
-	const recipeOne = document.createElement('img');
-	recipeOne.classList.add('recipe-one');
-	recipeOne.src = mealOne;
-	const recipeOneInfo = document.createElement('p');
-	recipeOneInfo.innerText = 'info goes here';
+    const createMenuItem = (recipeNum, description) => {
+        let container = document.createElement('div');
+        container.classList.add('recipe-container');
+        let img = document.createElement('img');
+        img.src = `${recipeNum}`
+        let recipeInfo = document.createElement('p');
+        recipeInfo.innerText = `${description}`;
 
-	//recipe 1 appending elements
-	containerOne.appendChild(recipeOne);
-	containerOne.appendChild(recipeOneInfo);
+        container.appendChild(img);
+        container.appendChild(recipeInfo);
+        menuContainer.appendChild(container);
+    }
 
-	// recipe 2 element creation
-	const containerTwo = document.createElement('div');
-	containerTwo.classList.add('recipe-container', 'container-two');
-	const recipeTwo = document.createElement('img');
-	recipeTwo.classList.add('recipe-two');
-	recipeTwo.src = mealTwo;
-	const recipeTwoInfo = document.createElement('p');
-	recipeTwoInfo.innerText = 'info goes here';
+    const menuItemOne = createMenuItem(
+        mealOne,
+        'Tasty Roast Potato Salad, Dressed With Spinach, Sesame Seeds, Olive Oil & Cajun Spice'
+    );
 
-	//recipe 2 appending elements
-	containerTwo.appendChild(recipeTwo);
-	containerTwo.appendChild(recipeTwoInfo);
+    const menuItemTwo = createMenuItem(
+        mealTwo,
+        'Fabulous Bowl Shaped Potato Fritters, Stuffed With Cream Cheese And Chives'
+    );
 
-	// recipe 3 element creation
-	const containerThree = document.createElement('div');
-	containerThree.classList.add('recipe-container', 'container-three');
-	const recipeThree = document.createElement('img');
-	recipeThree.classList.add('recipe-three');
-	recipeThree.src = mealThree;
-	const recipeThreeInfo = document.createElement('p');
-	recipeThreeInfo.innerText = 'info goes here';
 
-	//recipe 3 appending elements
-	containerThree.appendChild(recipeThree);
-	containerThree.appendChild(recipeThreeInfo);
+    const menuItemThree = createMenuItem(
+        mealThree,
+        'Refreshing Roast Potato Salad With Broccoli, Corn, Carrots, Red Onions & Sesame Seeds'
+    )
 
-	// recipe 4 element creation
-	const containerFour = document.createElement('div');
-	containerFour.classList.add('recipe-container', 'container-four');
-	const recipeFour = document.createElement('img');
-	recipeFour.classList.add('recipe-four');
-	recipeFour.src = mealFour;
-	const recipeFourInfo = document.createElement('p');
-	recipeFourInfo.innerText = 'info goes here';
+	const menuItemFour = createMenuItem(
+        mealFour,
+        'Quick & Easy, Crispy, Home-Made Fries Seasoned With Peri-Peri Salt'
+    )
 
-	//recipe 4 appending elements
-	containerFour.appendChild(recipeFour);
-	containerFour.appendChild(recipeFourInfo);
+	const menuItemFive = createMenuItem(
+        mealFive,
+        'Delicious Crispy, Oven-Baked Curly Fries Seasoned With Paprika & Sweet Curry Powder, Served With Ketchup'
+    )
+	
+    const menuItemSix = createMenuItem(
+        mealSix,
+        'Healthy, Air-Fried Sweet Potato Fries Seasoned Lightly With Salt & Pepper. Served With Low Calories BBQ Sauce'
+    )
 
-	// recipe 5 element creation
-	const containerFive = document.createElement('div');
-	containerFive.classList.add('recipe-container', 'container-five');
-	const recipeFive = document.createElement('img');
-	recipeFive.classList.add('recipe-five');
-	recipeFive.src = mealFive;
-	const recipeFiveInfo = document.createElement('p');
-	recipeFiveInfo.innerText = 'info goes here';
 
-	//recipe 5 appending elements
-	containerFive.appendChild(recipeFive);
-	containerFive.appendChild(recipeFiveInfo);
-
-	// recipe 5 element creation
-	const containerSix = document.createElement('div');
-	containerSix.classList.add('recipe-container', 'container-six');
-	const recipeSix = document.createElement('img');
-	recipeSix.classList.add('recipe-six');
-	recipeSix.src = mealSix;
-	const recipeSixInfo = document.createElement('p');
-	recipeSixInfo.innerText = 'info goes here';
-
-	//recipe 5 appending elements
-	containerSix.appendChild(recipeSix);
-	containerSix.appendChild(recipeSixInfo);
-
-	//appending recipes to menu container
-	menuContainer.appendChild(containerOne);
-	menuContainer.appendChild(containerTwo);
-	menuContainer.appendChild(containerThree);
-	menuContainer.appendChild(containerFour);
-	menuContainer.appendChild(containerFive);
-	menuContainer.appendChild(containerSix);
 	container.appendChild(menuContainer);
 }
